aedd9c0d5941fa496a990f8a161c5c0830131233
otaviodecampos <otaviodecampos@hotmail.com>
2015-10-25 05:18:48 -0200

    Delete demo files

--- demo/server.js
+++
@@ -1,9 +0,0 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-var path = __dirname;
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-
-connect().use('/', serveStatic(path)).listen(port, ip, function() {
-    console.log('Web Server running on ' + ip + ':' + port + '...');
-});
