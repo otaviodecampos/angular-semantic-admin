069ec0279c9a1d7bfc9459b08a3063ee9a81634e
otaviodecampos <otaviodecampos@hotmail.com>
2017-07-09 23:43:52 -0300

    Updated semantic to 2.2.10

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var path = __dirname + '/dist-demo';
 var ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '127.0.0.1'
-var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 9090;
 
 connect().use('/', serveStatic(path)).listen(port, ip, function() {
     console.log('Web Server running on ' + ip + ':' + port + '...');
